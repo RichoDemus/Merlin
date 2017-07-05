32a7cb1d8e68fd51359773b88441aa29da2933d4
RichoDemus <richodemus@gmail.com>
2017-07-05 18:07:30 +0200

    Remove some old files

--- run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run --rm -it -p 80:80 --volume `pwd`:/src node:8.1.2-alpine sh
--- run_frontend_dev.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-docker run --rm -it -p 80:3000 --volume `pwd`/frontend:/src node:8.1.2 bash
