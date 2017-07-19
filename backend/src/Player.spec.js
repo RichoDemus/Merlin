34c1c8c0f4266828270c4424a7f3aea932386f89
RichoDemus <richodemus@gmail.com>
2017-07-19 13:34:09 +0200

    Fix Player.spec

--- backend/src/Player.spec.js
+++ backend/src/Player.spec.js
@@ -1,10 +1,10 @@
 import {describe, it} from "mocha";
 import Player from "./Player";
-const expect = require('chai').expect;
+import {expect} from "chai";
 
 describe("Test Room", () => {
     it("serializes to json without websocket", () => {
         const target = new Player("richo", "websocket-mock");
-        expect(JSON.stringify(target)).to.equal(JSON.stringify({name: "richo"}));
+        expect(JSON.stringify(target)).to.equal(JSON.stringify({name: "richo", lord: false}));
     });
 });
