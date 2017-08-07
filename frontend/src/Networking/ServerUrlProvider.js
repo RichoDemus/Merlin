a8a847a827c2b96465289e57db21608fa405a6df
RichoDemus <richodemus@gmail.com>
2017-08-07 16:23:30 +0200

    Add ability to set Backend URL at build time

--- frontend/src/Networking/ServerUrlProvider.js
+++ frontend/src/Networking/ServerUrlProvider.js
@@ -1,6 +1,7 @@
 export const serverUrl = () => {
-    // This doesn't work...
-    const env = process.env.REACT_APP_API_URL;
+    // This env is set during the build process in the Dockerfile
+    // There might be a better way, investigate :)
+    const env = process.env.REACT_APP_BACKEND_URL;
     const server = env || "ws://" + window.location.hostname + ":8080/websocket";
     console.log("Connecting to api at:", server);
     return server;
