4e51e7956932932e9dd8789c4f24ebc055f9e8f8
RichoDemus <richodemus@gmail.com>
2017-08-07 20:06:49 +0200

    Run npm install before test

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ set -e
 
 export CI=true
 
-(cd frontend && npm test)
-(cd backend && npm test)
+(cd frontend && npm install && npm test)
+(cd backend && npm install && npm test)
 
 docker build -t richodemus/merlin-backend:latest backend
 docker build -t richodemus/merlin-frontend:latest frontend
