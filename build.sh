dd3f282df13e5b8959b02180a105272e7016b4f5
RichoDemus <richodemus@gmail.com>
2017-08-07 16:13:45 +0200

    Run tests before building docker images

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+export CI=true
+
+(cd frontend && npm test)
+(cd backend && npm test)
+
 docker build -t richodemus/merlin-backend:latest backend
 docker build -t richodemus/merlin-frontend:latest frontend
 
